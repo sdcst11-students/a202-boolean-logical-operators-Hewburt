fa905d403aeb80c06bc088137cc711d87057b239
Hewburt <144651080+Hewburt@users.noreply.github.com>
2023-11-09 13:30:16 -0800

    almost done

--- task1.py
+++ task1.py
@@ -32,8 +32,8 @@ Enter the distance from the sun in AU: 2
 That is not within normal Earth or Mars orbit.
 
 """
-x = float(input("Enter the distance from the sun in AU: 9.4"))
-if x<= 0.9759 or x>= 1.016:
+x = float(input("Enter the distance from the sun in AU:"))
+if  (0.9759 < x<= 1.016) or (1.524 < x <= 1.666):
     print("That is within normal Earth or Mars orbit\n")
 else:  
     print("That is not within normal Earth or Mars orbit\n")
